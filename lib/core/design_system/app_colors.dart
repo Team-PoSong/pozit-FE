64dd4872eb4a7032fbb457ff4f64630ef3610116
Hyoun-Young Cho <hyoun-youngcho@Hyoun-Youngui-MacBookAir.local>
2026-07-09 03:08:50 +0900

    feat: #4 디자인 시스템 컬러 및 타이포그래피 정의

--- lib/core/design_system/app_colors.dart
+++ lib/core/design_system/app_colors.dart
@@ -1,1 +1,24 @@
-// DS에 정의된 색상을 변수화합니다.
+import 'package:flutter/material.dart';
+
+/// 앱 전역에서 사용하는 컬러 상수 정의
+class AppColors {
+  const AppColors._();
+
+  static const Color primary = Color(0xFF4649FF);
+
+  static const Color purple3 = Color(0xFF6669FF);
+  static const Color purple2 = Color(0xFF9FA1FF);
+  static const Color purple1 = Color(0xFFE4E4FF);
+
+  static const Color gray5 = Color(0xFF7E7E7E);
+  static const Color gray4 = Color(0xFFC6C6C6);
+  static const Color gray3 = Color(0xFFE3E3E3);
+  static const Color gray2 = Color(0xFFF5F5F5);
+  static const Color gray1 = Color(0xFFFAFAFA);
+
+  static const Color error = Color(0xFFFF0000);
+
+  static const Color white = Color(0xFFFFFFFF);
+
+  static const Color text = Color(0xFF161424);
+}
