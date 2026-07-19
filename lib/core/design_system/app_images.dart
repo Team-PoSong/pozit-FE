89767fcb0e835c5d73f83669958d27ff6df4f6f5
Hyoun-Young Cho <hyoun-youngcho@Hyoun-Youngui-MacBookAir.local>
2026-07-20 02:01:24 +0900

    feat: #6 여행 카드 컴포넌트 추가

--- lib/core/design_system/app_images.dart
+++ lib/core/design_system/app_images.dart
@@ -12,9 +12,11 @@ class AppImages {
   static const String city = '$_basePath/img_city.png';
   static const String pin = '$_basePath/img_pin.png';
   static const String ticket = '$_basePath/img_ticket.png';
+  static const String travelMockup = '$_basePath/img_travel_mockup.png';
 
   static const String posongCarrier = '$_basePath/img_posong_carrier.png';
-  static const String posongCarrierMini = '$_basePath/img_posong_carrier_mini.png';
+  static const String posongCarrierMini =
+      '$_basePath/img_posong_carrier_mini.png';
   static const String posongPlain = '$_basePath/img_posong_plain.png';
   static const String posongPlainMini = '$_basePath/img_posong_plain_mini.png';
   static const String posongSleeping = '$_basePath/img_posong_sleeping.png';
