3cc13f9dc14531b24659c498784bb0485305b770
Hyoun-Young Cho <hyoun-youngcho@Hyoun-Youngui-MacBookAir.local>
2026-07-22 00:21:17 +0900

    feat: #22 카카오 로그인 딥링크 연동

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:pozit/core/auth/auth_deep_link.dart';
 import 'package:pozit/core/design_system/app_colors.dart';
 import 'package:pozit/screens/auth/login_screen.dart';
 
@@ -7,14 +8,45 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final GlobalKey<ScaffoldMessengerState> _messengerKey =
+      GlobalKey<ScaffoldMessengerState>();
+  late final AuthDeepLinkListener _authDeepLinkListener;
+
+  @override
+  void initState() {
+    super.initState();
+    _authDeepLinkListener = AuthDeepLinkListener()
+      ..listen(onLoginCode: _handleLoginCode);
+  }
+
+  void _handleLoginCode(String loginCode) {
+    _messengerKey.currentState
+      ?..hideCurrentSnackBar()
+      ..showSnackBar(const SnackBar(content: Text('로그인 정보를 확인하고 있어요.')));
+
+    // loginCode 교환 API가 확정되면 여기서 accessToken으로 교환한다.
+  }
+
+  @override
+  void dispose() {
+    _authDeepLinkListener.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pozit',
       debugShowCheckedModeBanner: false,
+      scaffoldMessengerKey: _messengerKey,
       theme: ThemeData(
         scaffoldBackgroundColor: AppColors.white,
         fontFamily: 'Pretendard',
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import app_links
 import file_selector_macos
 import flutter_secure_storage_darwin
 import google_sign_in_ios
@@ -12,6 +13,7 @@ import path_provider_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
