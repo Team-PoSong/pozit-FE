6470da6cee9da1496795c43781554c95f9920070
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-07-22 23:20:30 +0900

    feat: #22 main.dart에dio_client 주입

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
+// TODO: import 경로 변경 필요!!!!!!!
+import 'package:pozit/core/auth/auth_token_storage.dart';
 import 'package:pozit/core/config/app_config.dart';
 import 'package:pozit/core/design_system/app_colors.dart';
+import 'package:pozit/core/network/dio_client.dart';
 import 'package:pozit/screens/auth/login_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await KakaoSdk.init(nativeAppKey: AppConfig.kakaoNativeAppKey);
+
+  DioClient.instance.attachAccessTokenProvider(
+    () => const AuthTokenStorage().readAccessToken(),
+  );
+
   runApp(const MyApp());
 }
 
