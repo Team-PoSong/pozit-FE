89681962d50fe5fa4f2aac628255eff064ed9fdf
Hyoun-Young Cho <hyoun-youngcho@Hyoun-Youngui-MacBookAir.local>
2026-07-24 02:35:28 +0900

    refactor: #22 온보딩 플로우와 프리뷰 통합

--- lib/screens/onboarding/onboarding_flow_screen.dart
+++ lib/screens/onboarding/onboarding_flow_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widget_previews.dart';
 
 import '../home/home_screen.dart';
 import 'nickname_screen.dart';
@@ -42,4 +43,18 @@ class _OnboardingFlowScreenState extends State<OnboardingFlowScreen> {
       },
     );
   }
+}
+
+@Preview(group: 'hycho', name: 'Onboarding Flow', size: Size(393, 852))
+Widget onboardingFlowPreview() {
+  return const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MediaQuery(
+      data: MediaQueryData(
+        size: Size(393, 852),
+        padding: EdgeInsets.only(top: 59, bottom: 34),
+      ),
+      child: OnboardingFlowScreen(),
+    ),
+  );
 }
