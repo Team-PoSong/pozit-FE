18332275efffe7d743617ed90d16c70c3d4c4354
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-08-14 14:24:26 +0900

    del: #41 주석 삭제

--- lib/screens/travel_settings/travel_settings_screen.dart
+++ lib/screens/travel_settings/travel_settings_screen.dart
@@ -49,11 +49,8 @@ class TravelSettingsResult {
   final List<int> tagIds;
   final bool isPublic;
 
-  /// 여행명/기간/태그/배경사진 중 하나 이상이 바뀌었는지 여부.
-  /// false면 [travelName], [startDate], [endDate], [tagIds]는 저장 대상이 아니다.
   final bool hasCoreFieldChanges;
 
-  /// 공개 설정이 바뀌었는지 여부.
   final bool isPublicChanged;
   final File? backgroundImage;
 }
